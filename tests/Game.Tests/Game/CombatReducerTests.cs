00b31535460e272bd702b9a4ef576fb70a4cf56e
NearDrive <NearDrive@gmail.com>
2026-03-10 15:14:04 +0100

    Make overflow block test independent of turn pacing

--- tests/Game.Tests/Game/CombatReducerTests.cs
+++ tests/Game.Tests/Game/CombatReducerTests.cs
@@ -175,22 +175,17 @@ public class CombatReducerTests
     [Fact]
     public void EndTurn_IsBlocked_WhenOverflowDiscardPending()
     {
-        var (current, _) = GameReducer.Reduce(GameState.Initial, new BeginCombatAction(CreateStableOpeningCombat(), Content.CardDefinitions));
+        var overflowState = CreateOverflowState(requiredDiscardCount: 1, handSize: 8);
 
-        for (var i = 0; i < 8 && current.Combat is { NeedsOverflowDiscard: false }; i++)
-        {
-            current = GameReducer.Reduce(current, new EndTurnAction()).NewState;
-        }
+        Assert.True(overflowState.Combat!.NeedsOverflowDiscard);
+        Assert.Equal(1, overflowState.Combat.RequiredOverflowDiscardCount);
 
-        Assert.True(current.Combat!.NeedsOverflowDiscard);
-        Assert.Equal(1, current.Combat.RequiredOverflowDiscardCount);
-
-        var (blockedState, blockedEvents) = GameReducer.Reduce(current, new EndTurnAction());
-        Assert.Equal(current, blockedState);
+        var (blockedState, blockedEvents) = GameReducer.Reduce(overflowState, new EndTurnAction());
+        Assert.Equal(overflowState, blockedState);
         Assert.Empty(blockedEvents);
 
-        var handCountBeforeDiscard = current.Combat.Player.Deck.Hand.Count;
-        var (resolvedState, discardEvents) = GameReducer.Reduce(current, new DiscardOverflowAction([0]));
+        var handCountBeforeDiscard = overflowState.Combat.Player.Deck.Hand.Count;
+        var (resolvedState, discardEvents) = GameReducer.Reduce(overflowState, new DiscardOverflowAction([0]));
 
         Assert.False(resolvedState.Combat!.NeedsOverflowDiscard);
         Assert.Equal(handCountBeforeDiscard - 1, resolvedState.Combat.Player.Deck.Hand.Count);
