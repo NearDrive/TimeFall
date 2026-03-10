3c9f96b67cf8c3ba5521ecf7c2e32aae548af666
NearDrive <NearDrive@gmail.com>
2026-03-10 20:57:49 +0100

    Restore CombatState Enemy compatibility for with-expressions

--- src/Game.Core/Combat/CombatState.cs
+++ src/Game.Core/Combat/CombatState.cs
@@ -18,22 +18,26 @@ public sealed record CombatState(
     int LastCardDamageDealt = 0)
 {
     public CombatState(
-        TurnOwner turnOwner,
-        CombatEntity player,
-        CombatEntity enemy,
-        bool needsOverflowDiscard,
-        int requiredOverflowDiscardCount,
-        int attacksPlayedThisTurn = 0,
-        bool playedAttackThisTurn = false,
-        int nextAttackBonusDamageThisTurn = 0,
-        bool nextAttackDoubleThisTurn = false,
-        int allAttacksBonusDamageThisTurn = 0,
-        bool allAttacksDoubleThisTurn = false,
-        int lastCardMomentumSpent = 0,
-        int lastCardDamageDealt = 0)
-        : this(turnOwner, player, ImmutableList.Create(enemy), needsOverflowDiscard, requiredOverflowDiscardCount, attacksPlayedThisTurn, playedAttackThisTurn, nextAttackBonusDamageThisTurn, nextAttackDoubleThisTurn, allAttacksBonusDamageThisTurn, allAttacksDoubleThisTurn, lastCardMomentumSpent, lastCardDamageDealt)
+        TurnOwner TurnOwner,
+        CombatEntity Player,
+        CombatEntity Enemy,
+        bool NeedsOverflowDiscard,
+        int RequiredOverflowDiscardCount,
+        int AttacksPlayedThisTurn = 0,
+        bool PlayedAttackThisTurn = false,
+        int NextAttackBonusDamageThisTurn = 0,
+        bool NextAttackDoubleThisTurn = false,
+        int AllAttacksBonusDamageThisTurn = 0,
+        bool AllAttacksDoubleThisTurn = false,
+        int LastCardMomentumSpent = 0,
+        int LastCardDamageDealt = 0)
+        : this(TurnOwner, Player, ImmutableList.Create(Enemy), NeedsOverflowDiscard, RequiredOverflowDiscardCount, AttacksPlayedThisTurn, PlayedAttackThisTurn, NextAttackBonusDamageThisTurn, NextAttackDoubleThisTurn, AllAttacksBonusDamageThisTurn, AllAttacksDoubleThisTurn, LastCardMomentumSpent, LastCardDamageDealt)
     {
     }
 
-    public CombatEntity Enemy => Enemies[0];
+    public CombatEntity Enemy
+    {
+        get => Enemies[0];
+        init => Enemies = Enemies.Count == 0 ? ImmutableList.Create(value) : Enemies.SetItem(0, value);
+    }
 }
