5b481435f37eece63d99580cd4f036f59b0b3a29
NearDrive <NearDrive@gmail.com>
2026-04-14 19:29:47 +0200

    Add Phase 1 client screen manager and debug switching

---
+++ src/Game.Client/Screens/MapScreen.cs
@@ -0,0 +1,16 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game.Client.Screens;
+
+public sealed class MapScreen : IScreen
+{
+    public void Update(GameTime time)
+    {
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.GraphicsDevice.Clear(Color.ForestGreen);
+    }
+}
